dc87807a2cf1300d1b9e7511deec8d9e27d76713
ShubhamRSingh <shubhamrsingh9@gmail.com>
2021-08-05 19:50:13 +0530

    uploading mobile code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,39 @@ import { useLocation } from "react-router-dom";
 const Header = () => {
     const location = useLocation();
     // console.log("abc", location);
+
+    const burgerMenuClickHandler = (e) => {
+        //debugger;
+        e.currentTarget.classList.toggle("open");
+        const targetMenu = e.currentTarget.parentElement
+        targetMenu.classList.toggle("headerOpen");
+    }
+
+    const menuLinkClickHandler = (e) => {
+        //debugger;
+        console.log();
+        let targetHeader =  e.currentTarget.offsetParent;
+        targetHeader.classList.toggle('headerOpen');
+        let openMenu = document.querySelector('.headerOpen ul');
+        openMenu.nextElementSibling.classList.toggle('open');
+    }
+
     return(
             <div className="HeaderWrap">
                 <ul>
                     {
                         pageDetails.map((pageValue, pageIdx) => {
                             return (
-                                <li key={pageIdx} className={location.pathname === pageValue.pageUrl ? 'active' : ''}><Link to={pageValue.pageUrl}>{pageValue.pageName}</Link></li>
+                                <li key={pageIdx} className={location.pathname === pageValue.pageUrl ? 'active' : ''}><Link onClick={menuLinkClickHandler} to={pageValue.pageUrl}>{pageValue.pageName}</Link></li>
                             )
                         })
                     }
                 </ul>
+                <div id="menu-toggle" className="onlyMob" onClick={burgerMenuClickHandler}>
+                    <div className="bar bar1"></div>
+                    <div className="bar bar2"></div>
+                    <div className="bar bar3"></div>
+                </div>
             </div>
     )
 }
